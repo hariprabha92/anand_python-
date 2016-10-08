c8eb7a453c68530c75453fdd6be4e5e0360fa9e8
Hariprabha P <hariprabha.haridas@gmail.com>
2016-10-08 20:25:55 +0530

    Update problem13.py

--- chapter2/problem13.py
+++ chapter2/problem13.py
@@ -1,4 +1,8 @@
 ''' Write a function lensort to sort a list of strings based on length.'''
 def lensort(l):
-	l.sort(key=lambda x:len(l[x]))
+	l.sort(key=lambda x:len(x)
+	return l
+	
+	
 
+print lensort(['python', 'perl', 'java', 'c', 'haskell', 'ruby'])
