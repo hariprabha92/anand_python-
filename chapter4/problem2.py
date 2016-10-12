cc202cbe27bf1992990bb594b65ec2ac5388e7a3
Hariprabha P <hariprabha.haridas@gmail.com>
2016-10-12 22:08:42 +0530

    Add files via upload

---
+++ chapter4/problem2.py
@@ -0,0 +1,18 @@
+''' What will be the output of the following program?'''
+
+try:
+    print "a"
+except:
+    print "b"
+else:
+    print "c"
+finally:
+    print "d"
+
+
+'''output
+--------
+a
+c
+d'''
+
