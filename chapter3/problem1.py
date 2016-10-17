d60fc9ee5708902222ea33eae51fc9060bed3861
Hariprabha P <hariprabha.haridas@gmail.com>
2016-10-17 15:41:58 +0530

    Add files via upload

---
+++ chapter3/problem1.py
@@ -0,0 +1,13 @@
+'''
+Problem 1: Write a program to list all files in the given directory.'''
+
+import os
+ 
+def files(d):
+    
+    return os.listdir(d)
+
+
+
+print(files("anand_python-"))
+
---
+++ chapter3/problem2.py
@@ -0,0 +1,7 @@
+''' Write a program extcount.py to count number of files for each extension in the given directory. The program should take a directory name as argument and print count and extension for each available file extension.
+
+$ python extcount.py src/
+14 py
+4 txt
+1 csv '''
+
