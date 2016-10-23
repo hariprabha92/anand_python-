b1a2a163c7986ce2391f3117e62ef2ee49e79762
Hariprabha P <hariprabha.haridas@gmail.com>
2016-10-23 20:12:39 +0530

    Update problem16.py

--- chapter2/problem16.py
+++ chapter2/problem16.py
@@ -1,6 +1,9 @@
 ''' Write a function extsort to sort a list of files based on extension.'''
 def extsort(l):
-	l=[a lambda x:x.split('.')]
+	l.sort(key=lambda x:xsplit(".")[-1])
 	return l
+
+
+
 print extsort(['a.c', 'a.py', 'b.py', 'bar.txt', 'foo.txt', 'x.c'])
 
